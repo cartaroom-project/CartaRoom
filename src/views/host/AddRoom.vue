eb6366749af41783f9f70574ee5c1d75f58d1758
Erwin <erwinberceles@gmail.com>
2019-05-10 12:45:46 -0700

    Half way done styling the add a room page

--- src/views/host/AddRoom.vue
+++ src/views/host/AddRoom.vue
@@ -1,24 +1,63 @@
 <template>
-  <div class="addRoom">
-    <p>Let's add a new room</p>
-    <input type="text" v-model="roomInfo.name" placeholder="Name"><br>
-    <input type="number" v-model="roomInfo.capacity" placeholder="Capacity"><br>
-    <input type="text" v-model="roomInfo.description" placeholder="Description"><br>
-    <input type="text" v-model="roomInfo.address" placeholder="Address"><br>
-    Open Time:<br />  <input type="time" v-model="roomInfo.openTime"><br /> 
-    Close Time:<br />  <input type="time" v-model="roomInfo.closeTime"><br />
-     <h3>Amenities:</h3>
-      <p>test: {{roomInfo.amenities}}</p>
-
-    <li v-for="amenity in amenities"  v-bind:key ="amenity['.key']">
-        <input type="checkbox" :id="amenity.offering" :value="amenity.offering" v-model="roomInfo.amenities"><br>
-        <label :for="amenity.offering">{{amenity.offering}}</label>
-    </li>
-<!--    <input type = "file" @click="uploadImage">-->
-      <br>
-    <button @click="addRoom">Add Room</button><br>
-    <router-link to="/home">Cancel</router-link>
-  </div>
+    <div class="addRoom">
+        <div class="banner">
+            <br>
+            <p class="banner_text">Let's add a new room</p>
+        </div>
+        <br>
+        <br>
+        <br>
+        <div class="add">
+            <div class="row">
+                <div class="column">
+                    <label>Name</label>
+                    <label>Capacity</label>
+                    <label>Description</label>
+                    <label>Location</label>
+                    <label>Open Time</label>
+                    <label>Close Time</label>
+                </div>
+                <div class="column">
+                    <input class="input_add" type="text" v-model="roomInfo.name" placeholder="name">
+                    <br>
+                    <input class="input_add" type="number" v-model="roomInfo.capacity" placeholder="capacity">
+                    <br>
+                    <input class="input_add" type="text" v-model="roomInfo.description" placeholder="description">
+                    <br>
+                    <input class="input_add" type="text" v-model="roomInfo.address" placeholder="location">
+                    <br />
+                    <input class="input_time" type="time" v-model="roomInfo.openTime">
+                    <br />
+                    <input class="input_time" type="time" v-model="roomInfo.closeTime">
+                    <br />
+                </div>
+            </div>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <div class="row">
+            <h3>Amenities:</h3>
+            <p>test: {{roomInfo.amenities}}</p>
+            <li v-for="amenity in amenities" v-bind:key="amenity['.key']">
+                <input type="checkbox" :id="amenity.offering" :value="amenity.offering" v-model="roomInfo.amenities">
+                <br>
+                <p :for="amenity.offering">{{amenity.offering}}</p>
+            </li>
+            </div>
+            <!--    <input type = "file" @click="uploadImage">-->
+            <br>
+            <button @click="addRoom">Add Room</button>
+            <br>
+            <router-link to="/home">Cancel</router-link>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -96,24 +135,96 @@
 
 
 <style scoped>
+    label {
+        margin-left: 30%;
+        margin-top: 20px;
+        margin-bottom: 15px;        
+        font-family: Roboto;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 25px;
+        line-height: 35px;
+        display: flex;
+        align-items: center;
+        color: #000000;
+}
+
+    .row {
+        display: flex;
+}
+    .column {
+        flex: 50%;
+        padding: 10px;
+        height: 300px; /* Should be removed. Only for demonstration */
+}
+    .banner_text {
+        font-family: Rajdhani;
+        font-style: normal;
+        font-weight: 600;
+        font-size: 110px;
+        line-height: 191px;
+        text-align: center;
+        color: #000000;
+}
+
+    .banner {
+        height: 450px;
+        width: 100%;
+        background-image: url(../../assets/banner/Host2.jpg);
+        background-repeat: no-repeat;
+        background-size: cover;
+}
+
     .sign-up {
         margin-top: 40px;
-    }
-    input {
+}
+
+    .input_add {
         margin: 10px 0;
-        width: 20%;
-        padding: 15px;
+        width: 725px;
+        padding-left: 15px;
+        background: #FFFFFF;
+        border: 0.25px solid #000000;
+        box-sizing: border-box;
+        border-radius: 10px;
+        font-family: Roboto;
+        font-style: normal;
+        font-weight: 300;
+        font-size: 20px;
+        line-height: 35px;
+        margin-right: 100px;
+}
+    .input_time{
+        margin: 10px 0;
+        width: 125px;
+        padding-left: 15px;
+        background: #FFFFFF;
+        border: 0.25px solid #000000;
+        box-sizing: border-box;
+        border-radius: 10px;
+        font-family: Roboto;
+        font-style: normal;
+        font-weight: 300;
+        font-size: 20px;
+        line-height: 35px;
+        margin-right: 700px;
     }
+
     button {
         margin-top: 10px;
         width: 10%;
         cursor: pointer;
-    }
-    span {
-        display: block;
-        margin-top: 20px;
-        font-size: 11px;
-    }
+}
+
+    .add {
+        margin-left: 25%;
+        padding-top: 44px;
+        background: rgba(218, 229, 227, 0.9);
+        border-radius: 15px;
+        width: 1200px;
+        height: 1000px;
+}
+
 </style>
 
 
--- src/views/host/Home.vue
+++ src/views/host/Home.vue
@@ -1,62 +1,77 @@
 <template>
-  <div class="home">
-      <h1>You are a Host</h1>
-      <h2>List of All rooms</h2>
-      <ul v-for="room of rooms" v-bind:key ="room['.key']">
-      Name: {{room.name}} <br />
-      Capacity: {{room.capacity}} <br /> 
-      Address: {{room.address}} <br />  
-      <button v-on:click="viewRoom(room.roomID)">View Room</button>
-      <br /></ul>
-  </div>
+    <div class="home">
+        <div class="banner">
+            <h1>You are a Host</h1>
+            <h2>List of All rooms</h2>
+        </div>
+        <ul v-for="room of rooms" v-bind:key ="room['.key']">
+            Name: {{room.name}} <br />
+            Capacity: {{room.capacity}} <br /> 
+            Address: {{room.address}} <br />  
+            <button v-on:click="viewRoom(room.roomID)">View Room</button>
+            <br />
+            
+        </ul>
+    </div>
 </template>
-
 <script>
-import firebase from 'firebase';
-import db from '@/firebase.js';
-
-// db.ref('rooms').once('value').then(function(snapshot) {
-//   console.log(snapshot.val());
-// }).catch((error) => {
-//   console.log(error);
-// });
-
-  var userID;
-  firebase.auth().onAuthStateChanged(function(user) {
-
-  if (user) {
-   // console.log(user.uid); //a@a.com = gbEw7s5ic1drxG3vgFWD3DAMb972
-    userID = user.uid;
-  } else {
-   // console.log("No user available"); 
-    userID = "null";
-  }
-});
-
-export default {
-  name: 'home',
-   data () {
-    return {
-      rooms: []
+    import firebase from 'firebase';
+    import db from '@/firebase.js';
+    
+    // db.ref('rooms').once('value').then(function(snapshot) {
+    //   console.log(snapshot.val());
+    // }).catch((error) => {
+    //   console.log(error);
+    // });
+    
+      var userID;
+      firebase.auth().onAuthStateChanged(function(user) {
+    
+      if (user) {
+       // console.log(user.uid); //a@a.com = gbEw7s5ic1drxG3vgFWD3DAMb972
+        userID = user.uid;
+      } else {
+       // console.log("No user available"); 
+        userID = "null";
+      }
+    });
+    
+    export default {
+      name: 'home',
+       data () {
+        return {
+          rooms: []
+        }
+       },
+        created () {
+            db.ref('rooms').orderByChild("hostID").equalTo(userID).once('value').then((snapshot) => {
+            this.rooms = [];
+            snapshot.forEach((doc) => {
+              this.rooms.push(doc.val());
+            }) 
+          }).catch((error) => {
+            console.log(error);
+          });
+        },
+      methods: {
+          viewRoom: function(id){
+          this.$router.push({
+            name: 'RoomViewHost',
+            params: { id: id }
+          })
+        }
+      }
     }
-   },
-    created () {
-        db.ref('rooms').orderByChild("hostID").equalTo(userID).once('value').then((snapshot) => {
-        this.rooms = [];
-        snapshot.forEach((doc) => {
-          this.rooms.push(doc.val());
-        }) 
-      }).catch((error) => {
-        console.log(error);
-      });
-    },
-  methods: {
-      viewRoom: function(id){
-      this.$router.push({
-        name: 'RoomViewHost',
-        params: { id: id }
-      })
-    }
-  }
-}
 </script>
+
+<style>
+    .banner{
+        height: 450px;
+        width: 100%;
+        background-image: url(../../assets/banner/Host.jpg);
+        background-repeat: no-repeat;
+        background-size:cover;
+        background-position:25%;
+        
+    }
+</style>
