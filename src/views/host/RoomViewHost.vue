bd98c01c6be937f0ecbde1000ea98ee41f94b97e
Jeevan Bains <harjeevanbains@gmail.com>
2019-05-13 11:27:49 -0700

    made a check to see if room has current bookings before deleting and changes room status based on that

--- src/views/host/RoomViewHost.vue
+++ src/views/host/RoomViewHost.vue
@@ -1,106 +1,151 @@
 <template>
-
-    <div class="editRoom">
-        <p>Room View</p>
-        <h1>Room: {{ roomInfo.name }}</h1>
-        <p>Name: {{ roomInfo.name }}</p>
-        <p>Capacity: {{ roomInfo.capacity }}</p>
-        <p>Description: {{ roomInfo.description }}</p>
-        <p>Address: {{ roomInfo.address }}</p>
-        <p>Open Time: {{ roomInfo.openTime }}</p>
-        <p>Close Time: {{ roomInfo.closeTime }}</p>
-        <p>Total Bookings: {{ roomInfo.bookingCounter }}</p>
-        <h3>Amenities:</h3>
-            <ul v-for="amenity in roomInfo.amenities"  v-bind:key ="amenity['.key']">
-            {{amenity}}
-            </ul>
-      <br />
-      <button v-on:click="editRoom(roomInfo.roomID)">Edit Room</button>
-      <button v-on:click="deleteRoom(roomInfo.roomID)">Delete Room</button><br />
+<div class="editRoom">
+    <p>Room View</p>
+    <h1>Room: {{ roomInfo.name }}</h1>
+    <p>Name: {{ roomInfo.name }}</p>
+    <p>Capacity: {{ roomInfo.capacity }}</p>
+    <p>Description: {{ roomInfo.description }}</p>
+    <p>Address: {{ roomInfo.address }}</p>
+    <p>Open Time: {{ roomInfo.openTime }}</p>
+    <p>Close Time: {{ roomInfo.closeTime }}</p>
+    <p>Total Bookings: {{ roomInfo.bookingCounter }}</p>
+    <h3>Amenities:</h3>
+    <ul v-for="amenity in roomInfo.amenities" v-bind:key="amenity['.key']">
+        {{amenity}}
+    </ul>
+    <br />
+    <button v-on:click="editRoom(roomInfo.roomID)">Edit Room</button>
+    <button v-on:click="deleteRoom(roomInfo.roomID)">Delete Room</button><br />
     <router-link to="/home">Cancel</router-link>
 </div>
-
 </template>
 
- <script>
-  import firebase from 'firebase';
-  import db from '@/firebase.js';
-  var createRoom = firebase.functions().httpsCallable('createRoom');
-  var hostDeleteRoom = firebase.functions().httpsCallable('hostDeleteRoom');
+<script>
+import firebase from 'firebase';
+import db from '@/firebase.js';
+var createRoom = firebase.functions().httpsCallable('createRoom');
+var hostDeleteRoom = firebase.functions().httpsCallable('hostDeleteRoom');
+var bookingStatus = firebase.functions().httpsCallable('bookingStatus');
 
-  var hostID;
-  var roomID = '1';
-  var storageRef = firebase.storage().ref();
 
-  firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    hostID = user.uid;
-  } else {
-    hostID = 'null';
-  }
+var hostID;
+var roomID = '1';
+var storageRef = firebase.storage().ref();
+
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        hostID = user.uid;
+    } else {
+        hostID = 'null';
+    }
 });
 
-  export default {
-      name: 'addRoom',
-      
-      data() {
-      return {
-          id: 0,
-          room: {},
-        amenities:[
-          {offering: 'Wifi'},
-          {offering: 'Projector'},
-          {offering: 'Whiteboard'},
-          {offering: 'Ethernet'},
-        ],
-          roomInfo:
-              {
-                  hostID: hostID,
-                  name: '',
-                  capacity: '',
-                  description: '',
-                  address: '',
-                  roomID: roomID,
-                  reserved: 'false',
-                  bookingCounter: 0,
-                  openTime:0,
-                  closeTime:0,
-                  amenities: [],
-                  bookingSlots: [{startingTime: '9:00',endingTime: '10:00'}]
-              }
-      }
+export default {
+    name: 'addRoom',
+
+    data() {
+        return {
+            id: 0,
+            room: {},
+            amenities: [{
+                    offering: 'Wifi'
+                },
+                {
+                    offering: 'Projector'
+                },
+                {
+                    offering: 'Whiteboard'
+                },
+                {
+                    offering: 'Ethernet'
+                },
+            ],
+            roomInfo: {
+                hostID: hostID,
+                name: '',
+                capacity: '',
+                description: '',
+                address: '',
+                roomID: roomID,
+                reserved: 'false',
+                bookingCounter: 0,
+                openTime: 0,
+                closeTime: 0,
+                amenities: [],
+                bookingSlots: [{
+                    startingTime: '9:00',
+                    endingTime: '10:00'
+                }]
+            }
+        }
     },
     async created() {
-            this.id = this.$route.params.id;
-            await createRoom({id: this.id, roomInfo: this.roomInfo}).then((result) => {
-                this.roomInfo = result.data.roomInfo
-            }).catch(function(error) {
-                console.log(error);
-            });
+        this.id = this.$route.params.id;
+        await createRoom({
+            id: this.id,
+            roomInfo: this.roomInfo
+        }).then((result) => {
+            this.roomInfo = result.data.roomInfo
+        }).catch(function (error) {
+            console.log(error);
+        });
     },
 
     methods: {
-      deleteRoom: function(id) {
-       if(window.confirm("Are you sure you want to delete this room?")){
-          hostDeleteRoom({id:id}).then(() => {
-          alert('Room Deleted!');
-          this.$router.go(-1);
-        })
+        deleteRoom: function (id) {
+
+            db.ref('currentBookings').orderByChild("room/roomID").equalTo(id).once('value').then((snapshot) => {
+                if (snapshot.exists()) {
+                    if (window.confirm("Room currently has bookings, do you want to continue?")) {
+                        if (window.confirm("Are you sure you want to delete this room?")) {
+                            hostDeleteRoom({
+                                id: id
+                            }).then(() => {
+                                //changes booking status to 'room deleted' and removes from current bookings
+                                snapshot.forEach((doc) => {
+                                    bookingStatus({
+                                        bookingID: doc.key,
+                                        roomID: id,
+                                        statMsg: 'room deleted'
+                                    });
+                                });
+                                alert('Room Deleted!');
+                                this.$router.go(-1);
+                            })
+                        }
+                    }
+                } else {
+                    if (window.confirm("Are you sure you want to delete this room?")) {
+                        hostDeleteRoom({
+                            id: id
+                        }).then(() => {
+                            alert('Room Deleted!');
+                            this.$router.go(-1);
+                        })
+                    }
+                }
+            });
+
+            // if(window.confirm("Are you sure you want to delete this room?")){
+            //   hostDeleteRoom({id:id}).then(() => {
+            //   alert('Room Deleted!');
+            //   this.$router.go(-1);
+            // })
+            // }
+        },
+
+        editRoom: function (id) {
+            this.$router.push({
+                name: 'EditRoom',
+                params: {
+                    id: id
+                }
+            })
         }
-    },  
-
-    editRoom: function(id){
-      this.$router.push({
-        name: 'EditRoom',
-        params: { id: id }
-      })
-    }
     },
-    }
-
+}
 </script>
 
- 
 <style scoped>
 .sign-up {
     margin-top: 40px;
