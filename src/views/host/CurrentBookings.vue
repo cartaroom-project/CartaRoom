0f51cf047106cc380a58a69b9a85dea708b5ddf8
Erwin James Berceles <erwin@DTC-Y49354.ad.bcit.ca>
2019-05-13 13:25:45 -0700

    Styled the Current booking page

--- src/views/host/CurrentBookings.vue
+++ src/views/host/CurrentBookings.vue
@@ -1,19 +1,32 @@
 <template>
 <div class="currentBookings">
-    <h1>You are a Host</h1>
-    <h2>List of Current Bookings</h2>
-    <ul>
-        <ul v-for="booking of bookings" v-bind:key="booking['.key']">
-            Room Name: {{booking.room.name}} <br />
-            Booker: {{booking.userEmail}}<br />
-            Host: {{booking.host}} <br />
-            Date: {{booking.date}} <br />
-            Start Time: {{booking.startTime}}:00 <br />
-            End Time: {{booking.endTime}}:00 <br /><br />
-            <!-- <button v-on:click="unbookRoom(booking)">Remove Booking</button> <br /> -->
-        </ul>
-    </ul>
-
+    <div class="banner">
+        <br>
+        <p class="banner_text">Current Bookings</p>
+    </div>
+    <div class="current" v-for="booking of bookings" v-bind:key="booking['.key']">
+        <div class="row">
+            <div class="column">
+                <img src="../../assets/banner/image.png">
+            </div>
+            <div class="column">
+                <label>Name</label>
+                <label>Booked By</label>
+                <label>Host</label>
+                <label>Date</label>
+                <label>Start Time</label>
+                <label>End Time</label>
+            </div>
+            <div class="column">
+                <p class="info">{{ booking.room.name }}</p>
+                <p class="info">{{ booking.userEmail }}</p>
+                <p class="info">{{ booking.host }}</p>
+                <p class="info">{{ booking.date }}</p>
+                <p class="info">{{ booking.startTime}}:00</p>
+                <p class="info">{{ booking.endTime}}:00</p>
+            </div>
+        </div>
+    </div>
 </div>
 </template>
 
@@ -58,3 +71,67 @@ firebase.auth().onAuthStateChanged(function (user) {
     }
 }
 </script>
+
+<style scoped>
+    img{
+    padding-top: 75px;
+    padding-left: 40px;
+    width: 75%;
+    }
+    .banner_text {
+    font-family: Rajdhani;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 110px;
+    line-height: 191px;
+    text-align: center;
+    color: #000000;
+    }
+    .banner {
+    height: 450px;
+    width: 100%;
+    background-image: url(../../assets/banner/Host2.jpg);
+    background-repeat: no-repeat;
+    background-size: cover;
+    }
+    label {
+    margin-left: 30%;
+    padding-top: 15px;
+    padding-bottom: 6px;
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 25px;
+    line-height: 35px;
+    display: flex;
+    align-items: center;
+    color: #000000;
+    }
+    .current{
+    margin-left: 25%;
+    padding-top: 45px;
+    padding-bottom: 45px;
+    margin-top: 45px;
+    margin-bottom: 45px;
+    background: rgba(218, 229, 227, 0.9);
+    border-radius: 15px;
+    width: 1200px;
+    }
+    .row {
+    display: flex;
+    }
+    .column {
+    flex: 33%;
+    padding: 10px;
+    }
+    .info{
+    border: 0.25px solid #000000;
+    width: 625px;
+    font-size: 20px;
+    line-height: 35px;
+    box-sizing: border-box;
+    background: #FFFFFF;
+    border-radius: 10px;
+    margin-right: 100px;
+    }   
+</style>
