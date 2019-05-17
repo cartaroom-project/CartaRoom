a38eb89b9fb387ff25c20f4ef971e898742dcc06
vncntstlm <vlam36@my.bcit.ca>
2019-05-17 13:44:04 -0700

    made banner opacity

--- src/views/host/AddRoom.vue
+++ src/views/host/AddRoom.vue
@@ -177,9 +177,11 @@ label {
 .banner {
     height: 450px;
     width: 100%;
-    background-image: url(../../assets/banner/Host2.jpg);
+    background: linear-gradient(rgba(255,255,255,.5), rgba(255,255,255,.5)),url("../../assets/banner/Host2.jpg");
     background-repeat: no-repeat;
     background-size: cover;
+    margin-top:-150px;
+    padding-top:150px;
 }
 
 .sign-up {
